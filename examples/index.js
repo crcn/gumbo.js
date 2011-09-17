0b6c9a2738b99baeae82bae0c659f323ecef1d86
Craig Condon <craig@spiceapps.com>
2011-09-16 21:25:48 -0500

    added dirty fs persistence

--- examples/index.js
+++ examples/index.js
@@ -1,11 +1,12 @@
 var gumbo = require('../lib/index'),
-col = gumbo.db({ async: false }).collection('google');
+col = gumbo.db({ async: false, persist: { 'fs': __dirname + '/data/' } }).collection('google');
 
 
 for(var i = 100; i--;)
 {
     col.insert({ name: 'craig', age: i }, function(){});
 }
+
 
 
 col.findOne({ name: 'craig', age: {$gt: 90 } }, function(err, item)
