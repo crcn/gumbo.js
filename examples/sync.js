42b089d4a97391128b149f059e0e31b04e045469
Craig Condon <craig@spiceapps.com>
2013-02-23 13:36:54 -0800

    ability to sync collections based on query

--- examples/sync.js
+++ examples/sync.js
@@ -1,19 +1,23 @@
 var gumbo = require("../");
 
 var source = gumbo.collection(),
-target = gumbo.collection();
+target = gumbo.collection(),
+target2 = gumbo.collection();
 
 
 source.syncTo(target);
+source.syncTo({ age: 21 }, target2);
 
 
 source.insert({ name: "craig" }).sync();
 
 console.log(target.findOne({ name: "craig" }).sync().get());
+console.log(target2.findOne({ name: "craig" }).sync())
 
 source.update({ name: "craig" }, {$set: { age: 21}}).sync();
 
 console.log(target.findOne({ name: "craig" }).sync().get());
+console.log(target2.findOne({ name: "craig" }).sync().get())
 
 
 
